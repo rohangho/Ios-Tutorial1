68a413512d74012b703002d7f48f6279d985d2b2
rohangho <roha@marsplay.co>
2020-05-23 15:36:40 +0530

    Initila setup

---
+++ Udemy1/Udemy1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Udemy1
+//
+//  Created by Rohan Ghosh on 23/05/20.
+//  Copyright © 2020 Rohan Ghosh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
