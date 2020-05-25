5456d9dfc1f19c1177d4df86f8c18cc83206672b
rohangho <roha@marsplay.co>
2020-05-25 14:11:32 +0530

    Added random

--- Udemy1/Udemy1/ViewController.swift
+++ Udemy1/Udemy1/ViewController.swift
@@ -10,6 +10,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var oneRand: Int=0
+    var twoRand: Int=0
+    
+    @IBOutlet weak var dice1: UIImageView!
+    @IBOutlet weak var dice2: UIImageView!
+    
+    @IBAction func roll(_ sender: Any) {
+        oneRand = Int.random(in: 1..<6)
+        twoRand = Int.random(in: 1..<6)
+        dice1.image=UIImage(named: "dice\(oneRand)")
+        dice2.image=UIImage(named: "dice\(twoRand)")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
