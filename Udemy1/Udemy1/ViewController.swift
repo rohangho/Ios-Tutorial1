d07528884e04a5c5a2c7fd03de73b3921646e0e3
rohangho <roha@marsplay.co>
2020-05-25 22:19:47 +0530

    Added the shake and array

--- Udemy1/Udemy1/ViewController.swift
+++ Udemy1/Udemy1/ViewController.swift
@@ -13,19 +13,35 @@ class ViewController: UIViewController {
     var oneRand: Int=0
     var twoRand: Int=0
     
+    //Arrays
+    let diceArray = ["dice1","dice2","dice3","dice4","dice5", "dice6"]
+    
     @IBOutlet weak var dice1: UIImageView!
     @IBOutlet weak var dice2: UIImageView!
     
-    @IBAction func roll(_ sender: Any) {
+    /**
+     Method to update the dice image
+     */
+    func updateDiceImage() {
         oneRand = Int.random(in: 1..<6)
         twoRand = Int.random(in: 1..<6)
-        dice1.image=UIImage(named: "dice\(oneRand)")
-        dice2.image=UIImage(named: "dice\(twoRand)")
+        dice1.image=UIImage(named: diceArray[oneRand])
+        dice2.image=UIImage(named: diceArray[twoRand])
+    }
+    
+    @IBAction func roll(_ sender: Any) {
+        updateDiceImage()
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        updateDiceImage()
         // Do any additional setup after loading the view.
+    }
+    
+    override func motionEnded(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
+        updateDiceImage()
+        
     }
 
 
